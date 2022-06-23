3d90b1bcbbb94b7e1561e0f725d9b26cdebc98cf
hUwUtao1 <urashiosu@gmail.com>
2022-06-23 15:52:53 +0700

    a

--- host.js
+++ host.js
@@ -2,12 +2,12 @@ const express = require("express")
 const server = express()
 
 server.all("/", (req, res) => {
-  res.send("Đã vào server thành công!")
+  res.send("[HOST] Đã tạo host thành công!")
 })
 
 function host() {
   server.listen(process.env.PORT || 3000, () => {
-    console.log("Đang tiến hành vào server")
+    console.log("[HOST] Đang tạo host....")
   })
 }
 
