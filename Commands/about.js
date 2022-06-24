dcc450293e31bca01341eb0fc68ea15189664119
hUwUtao1 <urashiosu@gmail.com>
2022-06-24 23:39:41 +0700

    a

--- Commands/about.js
+++ Commands/about.js
@@ -7,7 +7,8 @@ module.exports = {
     name: 'about',
     category: 'Client',
     description: 'Xem các thông tin của bot! 🤖',
-    run: (client, interaction) => {
+    run: async (client, interaction) => {
+        await interaction.deferReply()
         console.log(`[USED] ${interaction.user.username} đã sử dụng ${name}`)
         const createtime = client.user.createdAt
         const createday = new Date(createtime)
@@ -22,22 +23,20 @@ module.exports = {
         let seconds = Math.floor(totalSeconds % 60);
         const discordJSVersion = packageJSON.dependencies["discord.js"];
         let uptime = `${days} ngày, ${hours} giờ, ${minutes} phút và ${seconds} giây`;
-        readdirSync('./Commands/').forEach(dir => {
-        const length = readdirSync('./Commands').length
-        const embed = new MessageEmbed()
-            .setColor("RANDOM")
-            .setAuthor(`${client.user.tag}`, client.user.displayAvatarURL())
-            .setTitle(`Tất tần tận về ${client.user.tag}`)
-            .addField(`Tên bot: `, `${client.user.tag} ( ${client.user.id} )`)
-            .addField("Tổng lệnh:", `${length}`)
-            .addField(`Uptime:`, `${uptime}`)
-            .addField('Số server:', `${client.guilds.cache.size}`)
-            .addField('Sinh nhật mình:', `(<t:${createago}:F>)`)
-            .addField('Node.js version', `${process.version}`, true)
-            .addField("Discord.js version", `${discordJSVersion}`, true)
-            .setTimestamp()
-        //.setFooter(`Được yêu cầu bởi ${interaction.author.username} • ${client.user.username}`, client.user.displayAvatarURL({ dynamic: true }))
-        interaction.reply({ embeds: [embed], allowedMentions: { repliedUser: false } })
-        })
+            const length = readdirSync('./Commands').length
+            const embed = new MessageEmbed()
+                .setColor("RANDOM")
+                .setAuthor(`${client.user.tag}`, client.user.displayAvatarURL())
+                .setTitle(`Tất tần tận về ${client.user.tag}`)
+                .addField(`Tên bot: `, `${client.user.tag} ( ${client.user.id} )`)
+                .addField("Tổng lệnh:", `${length}`)
+                .addField(`Uptime:`, `${uptime}`)
+                .addField('Số server:', `${client.guilds.cache.size}`)
+                .addField('Sinh nhật mình:', `(<t:${createago}:F>)`)
+                .addField('Node.js version', `${process.version}`, true)
+                .addField("Discord.js version", `${discordJSVersion}`, true)
+                .setTimestamp()
+            //.setFooter(`Được yêu cầu bởi ${interaction.author.username} • ${client.user.username}`, client.user.displayAvatarURL({ dynamic: true }))
+            await interaction.editReply({ embeds: [embed] })
     }
 }
